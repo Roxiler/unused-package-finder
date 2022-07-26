0bb524ebaad4f593dc020ce96ce11695b09bc0bf
sai6855 <60743144+sai6855@users.noreply.github.com>
2022-07-26 10:51:55 +0530

    published

---
+++ README.md
@@ -0,0 +1,61 @@
+# unused-package
+
+**unused-package is a dependency analysing tool which lets you know which packages are not being used or if package is devDependency but installed as normal dependency**
+
+# Installation
+
+Using npm
+
+```
+npm install unused-package
+```
+
+Using yarn
+
+```
+yarn add unused-package
+```
+
+# Usage
+
+**create a file named unused.js with below code**
+
+```
+const check = require("unused-package");
+
+check({ entries: ['entry path to your code'] }).then((res) => {
+  console.log(res) // list of packages returned by library
+});
+
+Example:
+//for react projects
+check({ entries: ['src'] }).then((res) => {
+  console.log(res)
+});
+
+//for next.js projects
+check({ entries: ['pages','src','components','next.config.js'] }).then((res) => {
+  console.log(res)
+});
+```
+
+# Note:
+
+- if your code has mutiple entry paths, you can pass all entry points as array form to entries parameter
+- library returns the list of packages in array format. listed packages maybe not being used in code, or if devDependency is installed as normal dependency.
+
+**Include unused.js file in your build script to check for extra packages before every build step in package.json**
+
+```
+Example:
+//for react projects
+"scripts":{
+   "build": " node unused.js && react-scripts build",
+}
+//for next.js projects
+"scripts":{
+   "build": " node unused.js && next build",
+}
+```
+
+**Note: Currently package is still in initial phase and if library returns and false positives please feel free to open an issue**
---
+++ index.js
@@ -0,0 +1,120 @@
+const directories = __dirname.split("/");
+const nodeModulesIndex = directories.findIndex((dir) => dir === "node_modules");
+const startingPath =
+  nodeModulesIndex === -1
+    ? __dirname
+    : directories.filter((dir, i) => i < nodeModulesIndex).join("/");
+
+const package = require(`${startingPath}/package.json`);
+const fs = require("fs");
+
+const extensions = ["js", "jsx", "ts", "tsx", "cjs", "mjs"];
+
+const packagesInFiles = [];
+
+const traverseDirectories = async (source) => {
+  const isSourceFile = fs.lstatSync(source).isFile();
+  const data = isSourceFile
+    ? [{ isDirectory: () => false, name: source }]
+    : await fs.promises.readdir(source, { withFileTypes: true });
+
+  for (let i = 0; i < data.length; i++) {
+    const file = data[i];
+    if (file.isDirectory()) {
+      await traverseDirectories(`${source}/${file.name}`);
+    } else {
+      if (
+        extensions.some((extension) => file.name.trim().endsWith(extension))
+      ) {
+        const path = isSourceFile
+          ? `${startingPath}/${source}`
+          : `${startingPath}/${source}/${file.name}`;
+        const fileData = fs.readFileSync(path, "utf-8");
+        const codeLines = fileData.split("\n");
+        codeLines
+          .filter((line) => line.includes("import") || line.includes("require"))
+          .forEach((statement) => {
+            if (statement.trim().substring(0, 2) === "//") {
+              return;
+            }
+
+            if (statement.includes("from")) {
+              let packageName = statement.split("from")[1];
+              if (!packageName.trim().includes(".")) {
+                packageName = packageName
+                  .replace('"', "")
+                  .replace('"', "")
+                  .replace(";", "")
+                  .replace("'", "")
+                  .replace("'", "")
+                  .trim();
+
+                if (!packagesInFiles.includes(packageName)) {
+                  packagesInFiles.push(packageName);
+                }
+              }
+            }
+
+            if (statement.includes("require(")) {
+              let packageName = statement.split("require")[1];
+              if (!packageName.trim().includes(".")) {
+                packageName = packageName
+                  .replace("(", "")
+                  .replace(")", "")
+                  .replace(";", "")
+                  .replace('"', "")
+                  .replace('"', "")
+                  .replace("'", "")
+                  .replace("'", "")
+                  .trim();
+
+                if (!packagesInFiles.includes(packageName)) {
+                  packagesInFiles.push(packageName);
+                }
+              }
+            }
+          });
+      }
+    }
+  }
+};
+
+const findUnUsedPackages = async ({ entries = [] } = {}) => {
+  if (entries.length === 0) {
+    throw new Error("entries key cannot be empty");
+  }
+  for (const entry of entries) {
+    await traverseDirectories(entry);
+  }
+  const packageJsonDeps = Object.keys(package.dependencies);
+  const unusedPackages = [];
+
+  packageJsonDeps.forEach((package) => {
+    const isUsed = packagesInFiles.some((packageInFile) =>
+      packageInFile.startsWith(package.trim())
+    );
+
+    if (!isUsed) {
+      unusedPackages.push(package);
+    }
+  });
+
+  //check if anyone of unusedPackages is peerDependency for anyone of package
+
+  const removePeerDepPackages = unusedPackages.filter((package) => {
+    const isPeerDependency = packageJsonDeps.some((packageDep) => {
+      const packageJsonPath = `${startingPath}/node_modules/${packageDep}/package.json`;
+
+      const packageData = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+
+      return packageData.peerDependencies
+        ? !!packageData.peerDependencies[package]
+        : false;
+    });
+    return !isPeerDependency;
+  });
+
+  return removePeerDepPackages;
+};
+
+module.exports = findUnUsedPackages;
